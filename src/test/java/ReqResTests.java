f1d8ceb242d38a9ade1166c251c0db0aa373a37d
Carlos Oliva <colivap@devco.com.co>
2021-05-03 17:51:38 -0500

    Using the Get Method and accessing an element within another

--- src/test/java/ReqResTests.java
+++ src/test/java/ReqResTests.java
@@ -2,6 +2,8 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.Matchers.*;
+
 public class ReqResTests {
 
     @Test
@@ -21,7 +23,41 @@ public class ReqResTests {
                 .extract()
                 .asString();
 
-//        System.out.println(response);
+    }
+
+    @Test
+    public void loginTestWithAssetion() {
+
+        RestAssured
+                .given()
+                .log().all() //Para loggear lo que se está enviando
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "    \"email\": \"eve.holt@reqres.in\",\n" +
+                        "    \"password\": \"cityslicka\"\n" +
+                        "}")
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().all() //Para loggear lo que se recibe
+                .statusCode(200)
+                .body("token", equalToIgnoringCase("QpwL5tke4Pnpja7X4"));
 
     }
+
+    @Test
+    public void getSingleUserTest() {
+
+        RestAssured
+                .given()
+                .log().all() //Para loggear lo que se está enviando
+                .contentType(ContentType.JSON)
+                .get("https://reqres.in/api/users/2")
+                .then()
+                .log().all() //Para loggear lo que se recibe
+                .statusCode(200)
+                .body("data.id", equalTo(2));
+
+    }
+
+
 }
